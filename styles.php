4cc9a488039685fa977bb3dacbd58161f7813f4b
rgg7888 <80013616+rgg7888@users.noreply.github.com>
2021-04-18 12:52:47 -0600

    Create styles.php

---
+++ styles.php
@@ -0,0 +1,36 @@
+<?php
+
+echo implode("",
+
+    [
+        #Estilos Base
+            //example replace it !
+            vars([
+                "bitcoin-orange" => "#f7931a",
+                "soft-orange" => "#ffe9d5",
+                "secondary-blue" => "#1a9af7",
+                "soft-blue" => "#e7f5ff",
+                "warm-black" => "#201e1c",
+                "black" => "#282623",
+                "grey" => "#bababa",
+                "off-white" => "#FAF8F7",
+                "just-white" => "#fff"
+            ]),
+            sltr("*",["0border-box","10","20"]),
+            sltr("html",["362.5%","4'DM Sans', sans-serif"]),
+        #
+        #Header
+           //your code here
+        #
+        #Main
+            //your code here
+        #
+        #footer
+            //your code here
+        #
+        #media queries
+            //your code here
+        #
+    ]
+
+);
